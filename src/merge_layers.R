d9e10651ec8eab14bdee619de89e0fc90354ffc8
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-06-04 13:03:06 +0000

    DEV: merge mean and sd layers

---
+++ src/merge_layers.R
@@ -0,0 +1,16 @@
+#!/usr/bin/R
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# merge mean and sd layers in one GeoTIFF
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+library("stars")
+
+susc_m <- read_stars("dat/interim/susceptibility_mean.tif") |>
+  setNames("mean")
+susc_s <- read_stars("dat/interim/susceptibility_sd.tif") |>
+  setNames("sd")
+
+susc <- c(susc_m, susc_s) |>
+  merge(name = "metric")
+write_stars(obj = susc, dsn = "dat/processed/susceptility.tif")
