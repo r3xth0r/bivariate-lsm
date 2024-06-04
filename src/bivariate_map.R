e929c2dd365661cb558994c40fa794887ffabbc1
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-06-04 13:54:03 +0000

    DOC: improve docstrings

--- src/bivariate_map.R
+++ src/bivariate_map.R
@@ -15,7 +15,24 @@ suppressPackageStartupMessages({
   library("tictoc")
 })
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# helper functions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 sfc_as_cols <- function(x, geometry, names = c("x", "y"), drop_geometry = FALSE) {
+  #' Add geometry to dataframe as separate numeric columns.
+  #'
+  #' @description Extracts the geometry of an sf-object with geometry type POINT
+  #' and adds it as dedicated columns. The geometry column can be dropped optionally.
+  #'
+  #' @param x sf-object to extract the geometry from.
+  #' @param geometry character. Name of the geometry column. The geometry is
+  #' guessed from the sf object using sf::st_geometry() if not provided.
+  #' @param names character. Names of the coordinates, defaults to `c("x", "y")`.
+  #' @param drop_geometry logical. Keep (default) or drop geometry column after extraction.
+  #'
+  #' @usage sfc_as_cols(x)
+  #' @return Input object with coordinates added as numeric columns.
   if (missing(geometry)) {
     geometry <- sf::st_geometry(x)
   } else {
@@ -34,29 +51,46 @@ sfc_as_cols <- function(x, geometry, names = c("x", "y"), drop_geometry = FALSE)
   out
 }
 
-res <- read_stars("dat/processed/susceptibility.tif") |>
-  st_as_sf(as_points = TRUE)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# explore color palettes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-# the most suitable palettes seem to be c("Brown", "PurpleOr", "GrPink", "DkViolet")
 dims <- 3
 pals <- c("Bluegill", "BlueGold", "BlueOr", "BlueYl", "Brown", "DkBlue", "DkCyan", "DkViolet", "GrPink", "PinkGrn", "PurpleGrn", "PurpleOr")
 pal <- pals[8]
 p_pals <- lapply(pals, bi_pal, dim = dims)
 p_biscale <- wrap_plots(p_pals)
-# ggsave(filename = "plt/biscale_pals.png", plot = p_biscale, width = 133, height = 100, units = "mm")
+ggsave(filename = "plt/biscale_pals.png", plot = p_biscale, width = 133, height = 100, units = "mm")
+# the most suitable palettes seem to be c("Brown", "PurpleOr", "GrPink", "DkViolet")
 
-# TODO: different class thresholds
-res_point <- readRDS("dat/biscale_test_aoi.rds") |>
-  rename(susceptibility = mean_susc, uncertainty = sd_susc)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# data preparation
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# TODO: different class thresholds [argument "style" in `biscale::bi_class()`]
+
+# sf w/ point
+res_point <- read_stars("dat/processed/susceptibility.tif") |>
+  st_as_sf(as_points = TRUE) |>
+  rename(susceptibility = mean, uncertainty = sd)
+
+# sf w/ poly
 res_poly <- res_point |>
   st_rasterize() |>
   st_as_sf() |>
   bi_class(x = susceptibility, y = uncertainty, style = "quantile", dim = dims)
+
+# simple tibble w/o geometry
 res_point <- res_point |>
   bi_class(x = susceptibility, y = uncertainty, style = "quantile", dim = dims) |>
-  sfc_as_cols(drop_geometry = TRUE)
+  sfc_as_cols(drop_geometry = TRUE) |>
+  as_tibble()
 
-# point
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# plotting
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# Alternative 1: plot points with geom_raster
 map_raster <- ggplot() +
   geom_raster(data = res_point, mapping = aes(x = x, y = y, fill = bi_class), show.legend = FALSE) +
   bi_scale_fill(pal = pal, dim = dims) +
@@ -80,7 +114,7 @@ tic()
 map_raster
 toc()
 
-# polygon
+# Alternative 2: plot polygons with geom_sf()
 # CAVE: this is substantially slower
 # map_sf <- ggplot() +
 #   geom_sf(data = res_poly, mapping = aes(fill = bi_class), lwd = 0, show.legend = FALSE) +
@@ -100,4 +134,4 @@ legend <- bi_legend(
 
 p <- map_raster + legend + plot_layout(widths = c(7, 1))
 
-ggsave(filename = "plt/test_map.png", plot = p, width = 220, height = 100, units = "mm")
+ggsave(filename = "plt/bivariate_map_R.png", plot = p, width = 220, height = 100, units = "mm")
