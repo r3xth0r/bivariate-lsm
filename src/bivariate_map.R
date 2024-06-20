b86def217e5b856e1d8e090759782de2f607c106
Matthias Schlögl <matthias.schloegl@geosphere.at>
2024-06-20 07:18:01 +0000

    ENH: support both default and custom breaks

--- src/bivariate_map.R
+++ src/bivariate_map.R
@@ -56,7 +56,7 @@ sfc_as_cols <- function(x, geometry, names = c("x", "y"), drop_geometry = FALSE)
   out
 }
 
-custom_bi_class <- function(dat, brks, style = "quantile", dim = 3) {
+custom_bi_class <- function(dat, brks = NULL, style = "quantile", dim = 3) {
   #' Create custom classes for bivariate maps
   #'
   #' @description Computes custom classes for bivariate maps based using
@@ -69,22 +69,29 @@ custom_bi_class <- function(dat, brks, style = "quantile", dim = 3) {
   #'
   #' @param dat data.frame-like object to add a new column `bi_class` to.
   #' @param brks numeric vector specifying class splits for the mean.
+  #' Defaults to breaks for the defined `style` if not supplied.
   #' @param style A string identifying the style used to calculate breaks.
   #' See `bi_class()` for details.
   #' @param dim integer denoting the dimensions of the palette.
   #' See `bi_class()` for details.
   #'
   #' @return Input object with new column `bi_class`.
-  dat |>
-    bi_class(x = susceptibility, y = uncertainty, style = style, dim = dim) |>
-    mutate(
-      bc_s = cut(susceptibility, breaks = brks, include.lowest = TRUE, dig.lab = 3),
-      bc_u = cut(uncertainty, breaks = classInt::classIntervals(
-        uncertainty, n = dim, style = style
-      )$brks, include.lowest = TRUE, dig.lab = 3)
-    ) |>
-    mutate(across(starts_with("bc_"), as.integer)) |>
-    tidyr::unite("bi_class", bc_s:bc_u, sep = "-")
+  if (is.null(brks)) {
+    cat(paste0("Using breaks of type `", style, "`\n"))
+    dat |>
+      bi_class(x = susceptibility, y = uncertainty, style = style, dim = dim)
+  } else {
+    cat(paste0("Using custom breaks for mean and breaks of type `", style, "` for sd\n"))
+    dat |>
+      mutate(
+        bc_s = cut(susceptibility, breaks = brks, include.lowest = TRUE, dig.lab = 3),
+        bc_u = cut(uncertainty, breaks = classInt::classIntervals(
+          uncertainty, n = dim, style = style
+        )$brks, include.lowest = TRUE, dig.lab = 3)
+      ) |>
+      mutate(across(starts_with("bc_"), as.integer)) |>
+      tidyr::unite("bi_class", bc_s:bc_u, sep = "-")
+  }
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
