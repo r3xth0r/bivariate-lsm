530240d3390ade795f3a7ebfd7040ab27edc9195
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2025-01-16 15:34:15 +0100

    DEP: drop tag_level

--- src/bivariate_map.R
+++ src/bivariate_map.R
@@ -35,7 +35,7 @@ allpals <- c("Bluegill", "BlueGold", "BlueOr", "BlueYl", "Brown", "DkBlue", "DkC
 selpals <- c("BlueGold", "BlueOr", "DkBlue", "DkViolet", "GrPink", "PurpleOr")
 pal <- "DkViolet"
 p_pals <- map(allpals, \(x) bi_pal(x, dim = dims) + ggtitle(x) + theme(text = element_text(family = "Source Sans Pro")))
-p_biscale <- wrap_plots(p_pals, tag_level = "keep")
+p_biscale <- wrap_plots(p_pals)
 ggsave(filename = "plt/biscale_pals.png", plot = p_biscale, width = 200, height = 150, units = "mm")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
